cd7162859ab85327cbd577aa427b8302785da4e5
leAyush98 <rod999lee@gmail.com>
2023-06-08 05:36:51 +0530

    final

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,35 @@
-var card = document.getElementById("card");
-var press = document.getElementById("press");
+var music = document.getElementById("music");
+var water = document.getElementById("water");
+var underwater = document.getElementById("underwater");
 
-card.addEventListener('mouseenter', function() {
-    press.style.visibility = 'visible';
-  });
-  
-card.addEventListener('mouseleave', function() {
-    press.style.visibility = 'hidden';
-  });
+var play_music = false
 
+music.addEventListener('click', function(){
+  if (play_music == false){
+      play_music = true;
+      water.play();
+      
+  }
+  else{
+    play_music = false;
+    water.pause();
+    underwater.pause();
+  } 
   
+});
+
+// background-image: url("/../../static/images/overlay.png"), linear-gradient(60deg, rgba(255, 165, 150, 0.5) 5%, rgba(0, 228, 255, 0.35)), url("../../images/bg.gif"); 
+const targetElement = document.getElementById('card');
+const bodyElement = document.body;
+
+targetElement.addEventListener('mouseleave', function() {
+  bodyElement.style.backgroundImage = 'url("/../../static/images/overlay.png"), linear-gradient(60deg, rgba(255, 165, 150, 0.5) 5%, rgba(0, 228, 255, 0.35)), url("../static/images/bg.gif")';
+  underwater.pause();
+  water.play();
+});
+
+targetElement.addEventListener('mouseenter', function() {
+  bodyElement.style.backgroundImage = 'url("/../../static/images/overlay.png"), linear-gradient(60deg, rgba(255, 165, 150, 0.5) 5%, rgba(0, 228, 255, 0.35)), url("../static/images/underwater.gif")';
+  water.pause();
+  underwater.play();
+});
