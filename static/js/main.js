5ce7c8512648614ec2ba1f1d891a1891adc388a3
leAyush98 <rod999lee@gmail.com>
2023-06-08 07:08:01 +0530

    card padding fix

--- static/js/main.js
+++ static/js/main.js
@@ -23,13 +23,19 @@ const targetElement = document.getElementById('card');
 const bodyElement = document.body;
 
 targetElement.addEventListener('mouseleave', function() {
-  bodyElement.style.backgroundImage = 'url("/../../static/images/overlay.png"), linear-gradient(60deg, rgba(255, 165, 150, 0.5) 5%, rgba(0, 228, 255, 0.35)), url("../static/images/bg.gif")';
+  bodyElement.style.backgroundImage = 'url("/../../static/images/overlay.png"), linear-gradient(60deg, rgba(255, 165, 150, 0.5) 5%, rgba(0, 228, 255, 0.35)), url("static/images/bg.gif")';
   underwater.pause();
-  water.play();
+  if (play_music){
+    water.play();
+  }
+  
 });
 
 targetElement.addEventListener('mouseenter', function() {
-  bodyElement.style.backgroundImage = 'url("/../../static/images/overlay.png"), linear-gradient(60deg, rgba(255, 165, 150, 0.5) 5%, rgba(0, 228, 255, 0.35)), url("../static/images/underwater.gif")';
+  bodyElement.style.backgroundImage = 'url("/../../static/images/overlay.png"), linear-gradient(60deg, rgba(255, 165, 150, 0.5) 5%, rgba(0, 228, 255, 0.35)), url("static/images/underwater.gif")';
   water.pause();
-  underwater.play();
+  if (play_music){
+    underwater.play();
+  }
+  
 });
